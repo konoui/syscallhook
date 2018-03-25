8f47bdbde383c32d8fb66300c61d8eafedf65a0a
watanabe <ce66034@meiji.ac.jp>
2018-03-25 17:02:56 +0900

    [fix] __NR_systemcall_num

--- syscall_hook.h
+++ syscall_hook.h
@@ -4,7 +4,7 @@
 #define debug(fmt, args...)	pr_info("CPU %d " fmt, smp_processor_id(), ##args)
 #define alert(fmt, args...)	pr_alert("CPU %d " fmt, smp_processor_id(), ##args)
 
-
+#include<asm/syscall.h>
 #define MSR_LSTAR	0xc0000082
 void (*syscall_handler)(void);
 long unsigned int original_dispatcher;
@@ -12,14 +12,9 @@ extern void __fake_syscall_dispatcher(void);
 
 static inline uint64_t get_dispatcher_from_msr(void)
 {
-	uint32_t low = 0, high = 0;
 	uint64_t address;
 
-	rdmsr(MSR_LSTAR,low, high);
-	address = 0;
-	address |= high;
-	address = address << 32;
-	address |= low;
+	rdmsrl(MSR_LSTAR, address);
 
 	return address;
 }
@@ -35,13 +30,11 @@ static inline void init_hook_syscall(void)
 static inline void hook_syscall(void)
 {
 	debug("hook\n");
+	uint32_t low;
 
-	uint32_t low = 0, dummy = 0, high = 0;
-
-	rdmsr(MSR_LSTAR, dummy, high);
 	low = (uint32_t) (((uint64_t)__fake_syscall_dispatcher) & 0xffffffff);
 
-	wrmsr(MSR_LSTAR, low, high);
+	wrmsr(MSR_LSTAR, low, 0xffffffff);
 }
 
 static inline void unhook_syscall(void)
@@ -62,26 +55,20 @@ static inline void unhook_syscall(void)
 ////////////////////////////////////////////////////////////////////
 DEFINE_PER_CPU(uint64_t, rsp_scratch);
 DEFINE_PER_CPU(uint64_t, syscall_num);
-#define CLONE_NUM	56
-#define FORK_NUM	57
-#define VFORK_NUM	58
-#define EXECVE_NUM	59
-#define EXIT_NUM	60
-#define EXIT_GROUP_NUM	231
 
 void pre_hooking(int num,  void *arg1, void *arg2)
 {
-	if (get_cpu_var(syscall_num) == CLONE_NUM && num == CLONE_NUM)
+	if (num == __NR_clone)
 		debug("clone hook\tchild_stack: %p\n", arg2);
-	else if (get_cpu_var(syscall_num) == FORK_NUM && num == FORK_NUM)
+	else if (num == __NR_fork)
 		debug("frok hook\n");
-	else if (get_cpu_var(syscall_num) == VFORK_NUM && num == VFORK_NUM)
+	else if (num == __NR_vfork)
 		debug("vfrok hook\n");
-	else if (get_cpu_var(syscall_num) == EXECVE_NUM  && num == EXECVE_NUM)
+	else if (num == __NR_execve)
 		debug("execve hook: %s\n", arg1);
-	else if (get_cpu_var(syscall_num) == EXIT_NUM && num == EXIT_NUM)
+	else if (num == __NR_exit)
 		debug("exit hook\n");
-	else if (get_cpu_var(syscall_num) == EXIT_GROUP_NUM && num == EXIT_GROUP_NUM)
+	else if (num == __NR_exit_group)
 		debug("exit_group hook\n");
 }
 /////////////////////////////////////////////////////////////////////
